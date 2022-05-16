a5bcb34a58678c07c8f2ac82054679583583da5c
Sid <sidcarrollworks@gmail.com>
2022-05-15 23:45:36 -0600

    Updated classnames on StandardNode

--- packages/studio/frontend/src/views/FlowEditor/Diagram2/NodeTypes/StandardNode.tsx
+++ packages/studio/frontend/src/views/FlowEditor/Diagram2/NodeTypes/StandardNode.tsx
@@ -16,38 +16,56 @@ interface StandardNodeData {
 export type OwnProps = WrapNodeProps<StandardNodeData>
 
 const StandardNode: FC<OwnProps> = ({ selected, dragging, data: { name, onEnter, onReceive, next } }) => {
+  
+  const getType = (action) => {
+    if (action.includes('say #!builtin_text')) return style['block-simple']
+    else if (action.includes('say #!builtin_carousel')) return style['block-complex']
+    return style.block
+  }
+
   return (
     <div className={cx(style.nodeContainer, { selected, dragging })}>
-      <Handle type="target" position={Position.Left} />
-      <h2>{name}</h2>
-      {onEnter && (
-        <>
-          <h4>On Enter</h4>
-          <div className={style.blocks}>
-            {onEnter.map((action) => (
-              <div className={style.block}>{metaFromAction(action).action}</div>
-            ))}
+      <div className={style.head}>
+        <div className={style.port}>
+          <div>
+            <h2>{name}</h2>
+            <Handle type="target" position={Position.Left} />
           </div>
-        </>
-      )}
-      {onReceive && (
-        <>
-          <h4>On Receive</h4>
-          <div className={style.blocks}>
-            {onReceive.map((action) => (
-              <div className={style.block}>{metaFromAction(action).action}</div>
-            ))}
-          </div>
-        </>
-      )}
-      <h4>Transitions</h4>
-      <div className={style.transitions}>
-        {next.map(({ condition, caption, node }) => (
-          <div className={style.transition}>
-            {caption || condition}
-            <Handle type="source" position={Position.Right} />
-          </div>
-        ))}
+        </div>
+      </div>
+      
+      <div className={style.content}>
+        {onEnter && (
+          <>
+            <h4>On Enter</h4>
+            <div className={style.blocks}>
+              {onEnter.map((action) => (
+                <div className={getType(action)}>{metaFromAction(action).action}</div>
+              ))}
+            </div>
+          </>
+        )}
+        {onReceive && (
+          <>
+            <h4>On Receive</h4>
+            <div className={style.blocks}>
+              {onReceive.map((action) => (
+                <div className={style.block}>{metaFromAction(action).action}</div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+      <div className={style.footer}>
+        <h4>Transitions</h4>
+        <div className={style.transitions}>
+          {next.map(({ condition, caption, node }) => (
+            <div className={style.transition}>
+              {caption || condition}
+              <Handle type="source" position={Position.Right} />
+            </div>
+          ))}
+        </div>
       </div>
       {/* <Handle type="target" position={Position.Right} /> */}
     </div>
